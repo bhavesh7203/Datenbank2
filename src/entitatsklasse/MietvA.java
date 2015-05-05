09f638e45055fb6687ccdf82cecab9f3e05cb286
Alexander Müller <mueller@brick-commerce.com>
2015-05-05 09:57:30 +0200

    no message

---
+++ src/entitatsklasse/MietvA.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entitatsklasse;
+
+import java.util.List;
+
+/**
+ *
+ * @author alexander
+ */
+public class MietvA {
+    int mietnr;
+    int knr;
+    String mietdat;
+    double gespreis;
+    String rdat;
+    double mwst;
+    double bruttopreis;
+    List<cont> contl;
+}
---
+++ src/entitatsklasse/cont.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entitatsklasse;
+
+/**
+ *
+ * @author alexander
+ */
+public class cont {
+    int contnr;
+    int ctnr;
+    String stao;
+    String edat;
+    
+}
